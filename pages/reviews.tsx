2afb954d7cf34c0321197cabf79d34041a17c9fc
Raja Ziafat <61820514+rajaziafat@users.noreply.github.com>
2026-04-13 20:16:08 +0500

    Create reviews page in pages/reviews.tsx

---
+++ pages/reviews.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReviewsPage = () => {
+    return (
+        <div>
+            <h1>Reviews</h1>
+            <p>This is the reviews page.</p>
+        </div>
+    );
+};
+
+export default ReviewsPage;
