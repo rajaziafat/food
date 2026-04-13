bff604a96a552db1b5b2662a0661003467693bc1
Raja Ziafat <61820514+rajaziafat@users.noreply.github.com>
2026-04-13 20:16:18 +0500

    Create recipes page

---
+++ pages/recipes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recipes = () => {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <p>This is the recipes page.</p>
+    </div>
+  );
+};
+
+export default Recipes;
