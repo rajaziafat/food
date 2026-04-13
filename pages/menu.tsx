e6bbb9cdce615f31f8f31105108bb6832765a3e0
Raja Ziafat <61820514+rajaziafat@users.noreply.github.com>
2026-04-13 20:16:23 +0500

    Create menu display page

---
+++ pages/menu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Menu } from './Menu';
+
+const MenuDisplay = () => {
+    return (
+        <div>
+            <h1>Menu Display</h1>
+            <Menu />
+        </div>
+    );
+};
+
+export default MenuDisplay;
